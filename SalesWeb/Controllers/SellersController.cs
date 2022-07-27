54379d024262912a977963930eba7772223a91ba
HeldersSanto <helder2903.ds@gmail.com>
2022-07-26 23:08:24 -0300

    SellerService e FindAll sincrono

--- SalesWeb/Controllers/SellersController.cs
+++ SalesWeb/Controllers/SellersController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Services;
 
 namespace SalesWeb.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _selerService;
+
+        public SellersController(SellerService ss)
+        {
+            _selerService = ss;
+        }
         public IActionResult Index()
         {
-            return View();
+            var list = _selerService.FindAll();
+            return View(list);
         }
     }
 }
